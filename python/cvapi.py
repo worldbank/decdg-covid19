5edd45cf6aabf6372d5f4eeb83914b7fee904e05
Tim Herzog <timgherzog@gmail.com>
2020-03-20 00:13:30 -0400

    Include subnational case data in API

--- python/cvapi.py
+++ python/cvapi.py
@@ -15,6 +15,7 @@ Usage:
 
 import requests
 import pandas as pd
+import numpy as np
 from wbgapi.economy import coder
 from datetime import datetime
 import os
@@ -25,27 +26,27 @@ options = docopt(__doc__)
 
 config = {
   'hdx_url': 'https://data.humdata.org/api/3/action/package_show?id=novel-coronavirus-2019-ncov-cases',
-  'build_date': datetime.strftime(datetime.now(), '%Y-%m-%dT%H:%M:%S'),
+  'build_date': datetime.strftime(datetime.utcnow(), '%Y-%m-%dT%H:%M:%S'),
   'build_dir': options['TARGET_DIR'],
 }
 
-def to_json(name, key, c, d, r):
+def to_json(c, d, r, **kwargs):
     global config
 
     data = {
         'meta': {
             'build_date': config['build_date'],
             'update_date': config['update_date'],
-            'iso': key,
-            'name': name,
         },
         'data': []
     }
 
+    data['meta'].update(kwargs)
+
     for i in c.index:
         ts = datetime.strptime(i, '%m/%d/%y')   # e.g., 3/12/20
 
-        confirmed, deaths, recovered = int(c[i]), int(d[i]), int(r[i])
+        confirmed, deaths, recovered = int(np.nan_to_num(c[i])), int(np.nan_to_num(d[i])), int(np.nan_to_num(r[i]))
         key = datetime.strftime(ts, '%Y/%m/%d')
         data['data'].append({'date': key, 'confirmed': confirmed, 'deaths': deaths, 'recovered': recovered})
 
@@ -61,27 +62,66 @@ config['update_date'] = datetime.strftime(meta_mod, '%Y-%m-%dT%H:%M:%S')
 # assume that Confirmed, Deaths, and Recoveries are the 1st-3rd datasets
 confirmed_url, deaths_url, recovery_url = map(lambda x: x['url'], ckan['result']['resources'][0:3])
 
-c_ = pd.read_csv(confirmed_url)
-d_ = pd.read_csv(deaths_url)
-r_ = pd.read_csv(recovery_url)
+manifest = {}
+c = pd.read_csv(confirmed_url).replace(0, np.nan).dropna(how='all', axis=1)
+d = pd.read_csv(deaths_url).replace(0, np.nan).dropna(how='all', axis=1)
+r = pd.read_csv(recovery_url).replace(0, np.nan).dropna(how='all', axis=1)
 
-# aggregate and transform so dates are rows
-drop_columns = ['Province/State', 'Lat', 'Long']
-c = c_.drop(columns=drop_columns).dropna(how='all', axis=1).groupby('Country/Region').sum().transpose()
-d = d_.drop(columns=drop_columns).dropna(how='all', axis=1).groupby('Country/Region').sum().transpose()
-r = r_.drop(columns=drop_columns).dropna(how='all', axis=1).groupby('Country/Region').sum().transpose()
+date_columns = list(filter(lambda x: x not in ['Lat', 'Long', 'Province/State', 'Country/Region'], c.columns))
 
-keys = []
-for key in c.columns:
+# this is the file name for subnational estimates
+c['stp_key'] = c['Province/State'].fillna('').str.replace(r'\W','').str.upper()
+
+# c, d & r aren't always in the same order, so we need to create a common index
+c['geokey'] = c['Province/State'].fillna('_') + ':' + c['Country/Region'].fillna('_')
+d['geokey'] = d['Province/State'].fillna('_') + ':' + d['Country/Region'].fillna('_')
+r['geokey'] = r['Province/State'].fillna('_') + ':' + r['Country/Region'].fillna('_')
+
+c.set_index('geokey', inplace=True)
+d.set_index('geokey', inplace=True)
+r.set_index('geokey', inplace=True)
+
+data = to_json(c.sum()[date_columns], d.sum()[date_columns], r.sum()[date_columns], iso='WLD', name='World')
+with open(os.path.join(config['build_dir'], 'world.json'), 'w') as fd:
+    json.dump(data, fd)
+
+# aggregate by country
+aggs = c.groupby('Country/Region').agg([np.min, np.max, 'count'])
+c2 = c.groupby('Country/Region').sum()[date_columns]
+d2 = d.groupby('Country/Region').sum()[date_columns]
+r2 = r.groupby('Country/Region').sum()[date_columns]
+
+for key in c2.index:
     iso = coder(key)
     if iso:
-        keys.append(iso)
+        manifest[iso] = {'name': key, 'locales': []}
         with open(os.path.join(config['build_dir'], iso + '.json'), 'w') as fd:
-            data = to_json(key, iso, c[key], d[key], r[key])
+            meta = dict(iso=iso, name=key)
+            if aggs.loc[key]['Province/State']['count'] == 0:
+                meta['lon'] = aggs.loc[key]['Long']['amin']
+                meta['lat'] = aggs.loc[key]['Lat']['amin']
+
+            data = to_json(c2.loc[key], d2.loc[key], r2.loc[key], **meta)
             json.dump(data, fd)
 
+# now write subnational data
+for key in c.dropna(subset=['Province/State']).index:
+    row = c.loc[key]
+    iso = coder(row['Country/Region'])
 
-data = to_json('World', 'WLD', c.sum(axis=1), d.sum(axis=1), r.sum(axis=1))
-data['meta']['countries'] = keys
-with open(os.path.join(config['build_dir'], 'all.json'), 'w') as fd:
-    json.dump(data, fd)
+    # we skip rows where the latest day is empty. This eliminates county-level records
+    # in the US where a handful of cases were recorded but later counted at the state level
+    if iso and not np.isnan(row[date_columns[-1]]):
+        manifest[iso]['locales'].append(os.path.join(iso, row['stp_key']))
+
+        try:
+            os.mkdir(os.path.join(config['build_dir'], iso))
+        except:
+            pass
+
+        with open(os.path.join(config['build_dir'], iso, row['stp_key'] + '.json'), 'w') as fd:
+            data = to_json(c.loc[key][date_columns], d.loc[key][date_columns], r.loc[key][date_columns], iso=iso, name=row['Province/State'], lat=row['Lat'], lon=row['Long'])
+            json.dump(data, fd)
+
+with open(os.path.join(config['build_dir'], 'manifest.json'), 'w') as fd:
+    json.dump(manifest, fd)
