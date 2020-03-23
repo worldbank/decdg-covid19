1434a3d1a06b647b9c30ebc4391113eab3c15e86
Tim Herzog <timgherzog@gmail.com>
2020-03-23 18:10:05 -0400

    Add View | API links to indicator table

--- python/cvyaml.py
+++ python/cvyaml.py
@@ -18,7 +18,7 @@ from docopt import docopt
 
 config = docopt(__doc__)
 
-ntable_columns = 2
+ntable_columns = 3
 
 data = yaml.safe_load(open(config['INPUT'], 'r'))
 if config['--type'] == 'json':
@@ -31,7 +31,13 @@ elif config['--type'] == 'html':
         print('<tr><th colspan={}>{}</th></tr>'.format(ntable_columns, html.escape(topic['topic'])))
         for k,v in topic['indicators'].items():
             # print('<tr><td>{0}</td><td><a href="https://data.worldbank.org/indicator/{0}">{1}</a></td></tr>'.format(k, html.escape(v)))
-            print('<tr><td><a href="https://data.worldbank.org/indicator/{0}">{1}</a></td></tr>'.format(k, html.escape(v)))
+            # print('<tr><td><a href="https://data.worldbank.org/indicator/{0}">{1}</a></td></tr>'.format(k, html.escape(v)))
+            cells = [
+              html.escape(v),
+              '<a href="https://data.worldbank.org/indicator/{}">View</a>'.format(k),
+              '<a href="https://api.worldbank.org/v2/en/country/all/indicator/{}?source=2&format=json">API</a>'.format(k),
+            ]
+            print('<tr>' + ''.join(map(lambda x: '<td>{}</td>'.format(x), cells)) + '</tr>')
 
     print('</table>')
 
